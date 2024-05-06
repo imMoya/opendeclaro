c1735e9335d1e51d9aadff43b17a0e1a37c26762
Ignacio Moya <imoyared@gmail.com>
2024-05-06 22:33:20 +0200

    feat(degiro.returns): start with FIFO method adaption

--- opendeclaro/degiro/returns.py
+++ opendeclaro/degiro/returns.py
@@ -1,17 +1,59 @@
+from datetime import datetime, timedelta
+from typing import Optional
+
 import polars as pl
 from polars import DataFrame
+
+from .utils import opposite_transaction
+
+
+class FIFO:
+    def __init__(self, row: dict, df: DataFrame, opp_df: Optional[DataFrame] = None):
+        self.row = row
+        self.df = df
+        self.opp_df = opp_df
+        self.first_opp_df()
+
+    def first_opp_df(self):
+        if self.opp_df == None:
+            self.opp_df = (
+                self.df.sort("value_date")
+                .filter(
+                    (pl.col("action") == opposite_transaction(self.row["action"]))
+                    & (pl.col("value_date") < self.row["value_date"])
+                )
+                .with_columns(
+                    pl.cumsum("number").sub(self.row["shares_effective"]).sub(pl.col("number")).alias("pending")
+                )
+                .filter(pl.col("pending") <= 0)
+                .with_columns(
+                    pl.when(abs(pl.col("pending")) > pl.col("number"))
+                    .then(pl.col("number"))
+                    .otherwise(abs(pl.col("pending")))
+                    .alias("shares_effective")
+                )
+            )
+        else:
+            pass
 
 
 # fmt: off
 class Returns:
-    def __init__(self, ds: DataFrame):
+    def __init__(self, ds: DataFrame, end_date: Optional[str] = None, start_date: Optional[str] = None):
         self.data = ds
+        self.end_date = end_date
+        self.start_date  = start_date
 
     def return_on_stock(self, isin: str):
         df = self.data.filter(pl.col("isin") == isin)
-        for row in df.iter_rows(named=True):
+        opp_df = None
+        for row in df.sort(pl.col("value_date")).iter_rows(named=True):
             stocks_before = self.get_stocks_purchased_before(row, df)
-            print(stocks_before)
+            if self.choose_compute_transaction(row, stocks_before) == True:
+                row["date_2m_limit"] = row["value_date"] + timedelta(days=60)
+                row["shares_effective"] = min(abs(stocks_before), row["number"])
+                opp_df = FIFO(row, df, opp_df)
+
 
     @staticmethod
     def get_stocks_purchased_before(row: dict, df: DataFrame) -> float:
