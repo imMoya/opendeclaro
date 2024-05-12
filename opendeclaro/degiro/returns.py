49da02b751552ae8145f699ae1aff3b477354b06
Ignacio Moya <imoyared@gmail.com>
2024-05-12 18:47:15 +0200

    docs(degiro.returns): add docstring to Returns and FIFO class methods

--- opendeclaro/degiro/returns.py
+++ opendeclaro/degiro/returns.py
@@ -2,16 +2,43 @@ from datetime import datetime, timedelta
 from typing import Optional
 
 import polars as pl
-from degiro.utils import filter_rowdate_inside_dates, opposite_transaction
 from polars import DataFrame
+
+from opendeclaro.degiro.utils import filter_rowdate_inside_dates, opposite_transaction
 
 
 class FIFO:
-    def __init__(self, row: dict, df: DataFrame, opp_df: Optional[DataFrame] = None):
+    def __init__(self, row: dict, df: DataFrame):
+        """Initialization of class
+
+        Parameters
+        ----------
+        row : dict
+            dictionary containing the row of a transaction of the stocks dataframe
+        df : DataFrame
+            dataframe containing stock transactions
+        """
         self.row = row
         self.df = df
 
-    def opp_df(self, opp_df: DataFrame = pl.DataFrame([])):
+    def opp_df(self, opp_df: DataFrame = pl.DataFrame([])) -> DataFrame:
+        """Computes the opposite transaction to the traded row (e.g. if the traded row is "sell", it returns a dataframe
+        containing "buy" transactions).
+        It creates a "pending" column with the pending stocks of the transaction which are available for next trades,
+        and a "shares_effective" column that defines which is the number of shares from the transaction effective to a
+        given trade.
+
+
+        Parameters
+        ----------
+        opp_df : DataFrame, optional
+            dataframe with the opposite transaction to the traded row, by default pl.DataFrame([])
+
+        Returns
+        -------
+        DataFrame
+            dataframe with the opposite transaction to the traded row, after the FIFO method is applied
+        """
         if opp_df.is_empty():
             opp_df_affected = (
                 self.df.sort("value_date")
@@ -72,13 +99,36 @@ class FIFO:
 
 # fmt: off
 class Returns:
-    def __init__(self, ds: DataFrame, end_date: Optional[str] = None, start_date: Optional[str] = None):
-        self.data = ds
+    def __init__(self, df: DataFrame, end_date: Optional[str] = None, start_date: Optional[str] = None):
+        """Initialization of Class
+
+        Parameters
+        ----------
+        df : DataFrame
+            dataframe containing stock transactions
+        end_date : Optional[str], optional
+            ending date from which valid transactions are filtered, by default None
+        start_date : Optional[str], optional
+            starting date from which valid transactions are filtered, by default None
+        """
+        self.df = df
         self.end_date = end_date
         self.start_date  = start_date
 
-    def return_on_stock(self, isin: str):
-        df = self.data.filter(pl.col("isin") == isin).with_columns(pl.col("number").alias("number_orig"))
+    def return_on_stock(self, isin: str) -> float:
+        """Compute the return of a given stock ISIN
+
+        Parameters
+        ----------
+        isin : str
+            ISIN of the stock traded
+
+        Returns
+        -------
+        float
+            return on the stock
+        """
+        df = self.df.filter(pl.col("isin") == isin).with_columns(pl.col("number").alias("number_orig"))
         opp_df = pl.DataFrame([])
         return_stock = 0
         for row in df.sort(pl.col("value_date")).iter_rows(named=True):
@@ -139,7 +189,27 @@ class Returns:
         return stocks_purchased_before - stocks_sold_before
     
     @staticmethod
-    def choose_compute_transaction(row: dict, stocks_before: float, start_date: Optional[str] = None, end_date: Optional[str] = None) -> bool:
+    def choose_compute_transaction(
+        row: dict, stocks_before: float, start_date: Optional[str] = None, end_date: Optional[str] = None
+    ) -> bool:
+        """Decide whether to compute the transaction or not
+
+        Parameters
+        ----------
+        row : dict
+            dictionary containing the row of a transaction of the stocks dataframe
+        stocks_before : float
+            number of stocks purchased (long if positive, short if negative, zero if no position)
+        start_date : Optional[str], optional
+            starting date from which valid transactions are filtered, by default None
+        end_date : Optional[str], optional
+            ending date from which valid transactions are filtered, by default None
+
+        Returns
+        -------
+        bool
+            True if return is to be computed, False otherwise
+        """
         date_inside_filter = filter_rowdate_inside_dates(row["value_date"], start_date, end_date)
         if (row["action"] == "sell") & (stocks_before <= 0):
             return False
