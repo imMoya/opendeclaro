2715a0e6e91d4e2b8339c84932d594c64fd80c59
Ignacio Moya <imoyared@gmail.com>
2024-05-20 17:54:17 +0200

    feat(degiro.dataprep): implement new method to add isin change on stock

--- opendeclaro/degiro/dataprep.py
+++ opendeclaro/degiro/dataprep.py
@@ -39,7 +39,8 @@ class DataPrep:
         df.write_csv("datasets/Account_data.csv")
         df_costs.write_csv("datasets/Account_costs.csv")
         df_curr_rate.write_csv("datasets/Account_curr_rate.csv")
-        return df.join(df_costs, on="id_order").join(df_curr_rate, left_on="id_order", right_on="id_order", how="left").sort("date", descending=True)
+        df_final = df.join(df_costs, on="id_order").join(df_curr_rate, left_on="id_order", right_on="id_order", how="left").sort("date", descending=True)
+        return df_final.with_columns(pl.lit(False).alias("unintended"))
     
     def prepare_involuntary_orders(self):
         df = (
@@ -49,28 +50,52 @@ class DataPrep:
                 (pl.col("id_order").str.lengths() == 0)
             )
             .with_columns(pl.lit(None).alias("commision").cast(pl.Float32, strict=False))
+            .with_columns(pl.lit(True).alias("unintended"))
         )
         return df.sort("date", descending=True)
     
     @property
     def stocks_orders(self):
-        return self.map_eur_curr_rate(
+        df_stocks = self.map_eur_curr_rate(
             pl.concat(
                 [self.prepare_id_orders(),
                 self.prepare_involuntary_orders().select(self.prepare_id_orders().columns)],
                 how="align"
-            ).sort("date", descending=True)
+            ).unique().sort("date", descending=True)
         )
+        df_stocks = self.add_isin_change_col(df_stocks)
+        return df_stocks
 
     
     @staticmethod
     def map_eur_curr_rate(df: DataFrame) -> DataFrame:
         return df.with_columns(
-            pl.when(pl.col("varcur").str.contains("EUR"))
+            pl.when(pl.col("cashcur").str.contains("EUR"))
             .then(1.0)
             .otherwise(pl.col("curr_rate"))
             .alias("curr_rate")
         )
+    
+    @staticmethod
+    def add_isin_change_col(df: DataFrame) -> DataFrame:
+        df_isin = df.filter(
+            (pl.col("unintended") == True) & 
+            (pl.col("desc").str.contains("CAMBIO DE ISIN"))
+        )
+        for _, data in df_isin.group_by("value_date", maintain_order=True):
+            for row in data.iter_rows(named=True):
+                isin_opp = data.filter(pl.col("isin")!=row["isin"]).select("isin")
+                df = df.with_columns(
+                    pl.when(
+                        (pl.col("isin") == row["isin"]) &
+                        (pl.col("action") != row["action"]) &
+                        (pl.col("value_date") > row["value_date"])
+                    )
+                    .then(isin_opp)
+                    .otherwise(None)
+                    .alias("isin_change")
+                )
+        return df
 
 
 # fmt:on
