594bc1df1c82b28b776d07dbac2a9b3cd1e7f158
Ignacio Moya <imoyared@gmail.com>
2024-05-21 18:50:54 +0200

    feat(degiro.utils): add filter_df_inside_dates method

--- opendeclaro/degiro/utils.py
+++ opendeclaro/degiro/utils.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 from typing import Optional
 
+import polars as pl
+from polars import DataFrame
+
 
 def opposite_transaction(action: str):
     opposite = {"buy": "sell", "sell": "buy"}
@@ -11,6 +14,26 @@ def opposite_transaction(action: str):
 def datestr_to_datetime(datestr: str):
     date_format = "%d/%m/%Y"
     return datetime.strptime(datestr, date_format)
+
+
+# fmt: off
+def filter_df_inside_dates(
+    df: DataFrame, col_name: str = "value_date", start_date: Optional[str] = None, end_date: Optional[str] = None
+) -> DataFrame:
+    if (start_date is None) and (end_date is None):
+        return df
+    elif (start_date is not None) and (end_date is None):
+        return df.filter(pl.col(col_name)>datestr_to_datetime(start_date))
+    elif (start_date is None) and (end_date is not None):
+        return df.filter(pl.col(col_name)<datestr_to_datetime(end_date))
+    elif (start_date is not None) and (end_date is not None):
+        return (
+            df.filter(
+                (pl.col(col_name)>datestr_to_datetime(start_date)) &
+                (pl.col(col_name)<datestr_to_datetime(end_date))
+            )
+        )
+# fmt: on
 
 
 def filter_rowdate_inside_dates(rowdate: datetime, start_date: Optional[str] = None, end_date: Optional[str] = None):
